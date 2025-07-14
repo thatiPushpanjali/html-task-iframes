aaeed77b9da2040a68c70c51f85339a6f8f03292
thatiPushpanjali <thatipushpanjali@gmail.com>
2025-07-14 16:54:44 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,38 @@
+1.What are variables and why are they called variables? Assign two numbers to variables 'a' and 'b' and print their sum.
+a=5
+b=10
+print(a+b)
+
+
+print("2.Write a python program to substract two numbers and print the result")
+num1=123
+num2=456
+print(num1-num2)
+
+
+3.Multiply two variables and store the result in a third variable.print all three.
+a=432
+b=536
+c=a*b
+print(c)
+
+4.Devide 10 by 3 and print the result with and without decimals.
+a=10
+b=3
+print(a/b)
+print(a//b)
+
+
+5.Use floor division'//' to devide 17 by 4.print the output
+a=17
+b=4
+print(a//b)
+
+
+6.Use the modulo operator '%' to check the remainder when 25 is devided by 6.
+a=25
+b=6
+print(a%b)
+
+
+
